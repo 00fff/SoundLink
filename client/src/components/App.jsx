8945defc14b57d6c2e817c13897be54c062a831b
00fff <harithsullaiman@gmail.com>
2024-07-14 14:41:20 -0400

    Succefully pulled Song information

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,16 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import UserList from "./UserList"
 
-import UserList from './UserList';
 function App() {
   const [artistInfo, setArtistInfo] = useState([]);
-  const [accessToken, setAccessToken] = useState('');
+  const [accessToken, setAccessToken] = useState();
+  const [currentlyPlaying, setCurrentlyPlaying] = useState();
+
+  useEffect(() => {
+    console.log(accessToken); // This will log the updated accessToken whenever it changes
+  }, [accessToken]);
+
   const fetchAPI = async () => {
     try {
       const response = await axios.get('http://127.0.0.1:8080/api/check', {
@@ -13,21 +18,39 @@ function App() {
         credentials: 'include', // Include cookies in the request
         mode: 'cors'
       });
-      
-      console.log(response.data); // Log the response data directly after setting state
-      setAccessToken(response.data)
+
+      const token = response.data;
+      setAccessToken(token);
+      console.log(token); // Log the token directly, this will work
+
+      if (token) {
+        try {
+          const current_song = await axios.get('http://127.0.0.1:8080/api/getCurrentSong', {
+            method: 'GET',
+            credentials: 'include', // Include cookies in the request
+            mode: 'cors',
+            headers: {
+              'Authorization': `Bearer ${token}` // Use the token variable here
+            }
+          });
+          const songdata = current_song.data
+          setArtistInfo(current_song.data);
+          console.log(songdata)
+        } catch (error) {
+          console.error('Error fetching currently playing song:', error);
+        }
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-
   useEffect(() => {
     fetchAPI();
   }, []);
 
   return (
     <>
-      {/* <UserList artistInfo={artistInfo} /> */}
+      <UserList artistInfo={artistInfo} />
     </>
   );
 }
